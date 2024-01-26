4ab2484feedfc99349c16b499ef7f83a79e3861e
Husnudu <157006352+Husnudu@users.noreply.github.com>
2024-01-26 13:33:48 +0400

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,7 +4,6 @@ from config import Config
 
  TG_BOT_TOKEN = "6913569473:AAE4zUbMO8VkRQgMxI7Ue8yWwP90DM_9yBQ"
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
     APP_ID = int(os.environ.get("APP_ID"))
     API_HASH = os.environ.get("API_HASH")
     ADMIN = int(os.environ.get("ADMIN"))
